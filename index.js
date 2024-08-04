4b45f1cc078a4678642e3645531f9def0a3e3e42
umarghafoor420 <164578000+umarghafoor420@users.noreply.github.com>
2024-08-05 03:34:53 +0500

    Add files via upload
    this is a cli number guessing  game

---
+++ index.js
@@ -0,0 +1,15 @@
+#!//usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([{
+        name: "usergussNumber",
+        type: "number",
+        message: "please guss a number between 1-10:",
+    }]);
+if (answer.usergussNumber === randomNumber) {
+    console.log("Congratulation! you gess righ number.");
+}
+else {
+    console.log("You Gess Wronge number!!");
+}
+console.log("");
